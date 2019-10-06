b7562126d333a195cc31d86682249e7b0cab2e19
Nicolas Mattelaer <nicolas.mattelaer@gmail.com>
2019-10-06 23:06:09 +0200

    Changed web title and icon + changed docker-compose file

--- frontend/src/backend-connect.js
+++ frontend/src/backend-connect.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "http://localhost:21798";
 
 export async function uploadFile(file) {
     let formData = new FormData();
